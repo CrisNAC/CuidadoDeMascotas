def310387a6fe18aa07664c4b35b39a717824995
CrisNAC <cristian.acuna@fiuni.edu.py>
2025-09-14 21:58:41 -0300

    Subiendo la base del proyecto

---
+++ src/test/java/org/example/cuidadodemascota/CuidadoDeMascotaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cuidadodemascota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuidadoDeMascotaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
