935865bd575794934e83b8897acc5055710fafc5
J-Kanami-PS <jazminpavon@fiuni.edu.py>
2025-09-20 18:42:04 -0300

    Agregando entidades en user

--- src/main/java/org/example/cuidadodemascota/commons/entities/user/Carer.java
+++ src/main/java/org/example/cuidadodemascota/commons/entities/user/Carer.java
@@ -1,4 +1,34 @@
 package org.example.cuidadodemascota.commons.entities.user;
 
-public class Carer {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.cuidadodemascota.commons.entities.base.AbstractEntity;
+import org.example.cuidadodemascota.commons.entities.credential.User;
+import org.example.cuidadodemascota.commons.entities.reservation.Reservation;
+import org.example.cuidadodemascota.commons.entities.service.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "carers")
+public class Carer extends AbstractEntity {
+
+    @OneToOne
+    @JoinColumn(name = "id_user", nullable = false, unique = true)
+    private User user;
+
+    @Column(name = "enum_availability_status", nullable = false, length = 50)
+    private String availabilityStatus;
+
+    // 1-N -> services
+    @OneToMany(mappedBy = "carer", fetch = FetchType.LAZY)
+    private Set<Service> services = new HashSet<>();
+
+    // 1-N -> reservations
+    @OneToMany(mappedBy = "carer", fetch = FetchType.LAZY)
+    private Set<Reservation> reservations = new HashSet<>();
 }
--- src/main/java/org/example/cuidadodemascota/commons/entities/user/Owner.java
+++ src/main/java/org/example/cuidadodemascota/commons/entities/user/Owner.java
@@ -1,4 +1,31 @@
 package org.example.cuidadodemascota.commons.entities.user;
 
-public class Owner {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.cuidadodemascota.commons.entities.base.AbstractEntity;
+import org.example.cuidadodemascota.commons.entities.credential.User;
+import org.example.cuidadodemascota.commons.entities.pet.Pet;
+import org.example.cuidadodemascota.commons.entities.reservation.Reservation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "owners")
+public class Owner extends AbstractEntity {
+
+    @OneToOne
+    @JoinColumn(name = "id_user", nullable = false, unique = true)
+    private User user;
+
+    // 1-N -> pets
+    @OneToMany(mappedBy = "owner", fetch = FetchType.LAZY)
+    private Set<Pet> pets = new HashSet<>();
+
+    // 1-N -> reservations
+    @OneToMany(mappedBy = "owner", fetch = FetchType.LAZY)
+    private Set<Reservation> reservations = new HashSet<>();
 }
